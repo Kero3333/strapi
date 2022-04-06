8c5f6473ef7a59a593ca2ddf2488ea2d5b4767b3
Kévin Robic <kevin.r@institutsolacroup.com>
2022-04-06 11:18:29 +0200

    postgre

--- config/database.js
+++ config/database.js
@@ -1,11 +1,17 @@
-const path = require('path');
+const parse = require("pg-connection-string").parse;
+const config = parse(process.env.DATABASE_URL);
 
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: "postgres",
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      user: config.user,
+      host: config.host,
+      port: config.port,
+      database: config.database,
+      password: config.password,
+      schema: "public",
+      ssl: { rejectUnauthorized: false },
     },
-    useNullAsDefault: true,
   },
 });
